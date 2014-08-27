ee8388f49200c9746fc8cc1690f132ce14300513
shadowmoonk <2002.05@163.com>
2014-08-27 19:39:40 +0800

    Signed-off-by: shadowmoonk <2002.05@163.com> 版本 3.1

--- 大数/HUGEINT.cpp
+++ 大数/HUGEINT.cpp
@@ -1,3 +1,36 @@
+/* .
+Copyright 2010-2014 ShadowMoonk.
+
+This file is part of the HUGEINT Library.
+
+The HUGEINT Library is free software; you can redistribute it and/or modify
+it under the terms of either:
+
+  * the GNU Lesser General Public License as published by the Free
+    Software Foundation; either version 3 of the License, or (at your
+    option) any later version.
+
+or
+
+  * the GNU General Public License as published by the Free Software
+    Foundation; either version 2 of the License, or (at your option) any
+    later version.
+
+or both in parallel, as here.
+
+The HUGEINT Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received copies of the GNU General Public License and the
+GNU Lesser General Public License along with the HUGEINT Library.  If not,
+see https://www.gnu.org/licenses/.  */
+
+/*
+大数类构造函数和赋值函数
+*/
+
 #include "StdAfx.h"
 #include "HUGEINT.h"
 
--- 大数/hugeinttask.h
+++ 大数/hugeinttask.h
@@ -1,3 +1,36 @@
+/* .
+Copyright 2010-2014 ShadowMoonk.
+
+This file is part of the HUGEINT Library.
+
+The HUGEINT Library is free software; you can redistribute it and/or modify
+it under the terms of either:
+
+  * the GNU Lesser General Public License as published by the Free
+    Software Foundation; either version 3 of the License, or (at your
+    option) any later version.
+
+or
+
+  * the GNU General Public License as published by the Free Software
+    Foundation; either version 2 of the License, or (at your option) any
+    later version.
+
+or both in parallel, as here.
+
+The HUGEINT Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received copies of the GNU General Public License and the
+GNU Lesser General Public License along with the HUGEINT Library.  If not,
+see https://www.gnu.org/licenses/.  */
+
+/*
+大数多线程任务映射类.
+主要通过 基类HUGEINTTASK进行多态散发任务.
+*/
 #include "stdafx.h"
 
 
